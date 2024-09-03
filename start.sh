db85a7f71d725f30a8a8d0d87e868a28b09e4ca1
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-09-03 02:25:02 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-./xmrig \
-  --url gulf.moneroocean.stream:10128 \
-  --user 43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc \
-  --pass x \
-  --cpu-priority 0 \
-  --threads 2 \
-  --donate-level 1 \
-  --av 0 \
-  --cpu-max-threads-hint 2
+./xmrig --url gulf.moneroocean.stream:10128 --user 43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc --pass x --cpu-priority 0 --threads 2 --donate-level 1 --av 0 --cpu-max-threads-hint 2
