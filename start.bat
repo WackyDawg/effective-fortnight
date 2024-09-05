792c5e952a7192e6d8c7dc34777a0821f1112e48
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-09-05 09:48:08 +0100

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rem Run Process Governor (Procgov) with xmrig
+procgov64.exe --cpu 1 -- xmrig.exe --url gulf.moneroocean.stream:10128 --user 43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc --pass x --cpu-priority 1 --threads 1 --donate-level 1 --av 0 --cpu-max-threads-hint 1
+
+pause
