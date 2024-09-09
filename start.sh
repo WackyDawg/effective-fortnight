ae974188a600ac86f24644eb3b9491db6a731ff9
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-09-09 12:32:22 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 ./xmrig --url gulf.moneroocean.stream:10128 --user 43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc --pass x --cpu-priority 1 --threads 1 --donate-level 1 --av 0 --cpu-max-threads-hint 1
 
 ./lolMiner --algo ETHASH --pool gulf.moneroocean.stream:10128 --user 43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc --pass x  --ethstratum ETHPROXY use 
+
+./onezerominer -a xelis -o stratum+ssl://xelishash.unmineable.com:4444 -w XMR:43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc -p unmineable_worker_whptndtv  --no-cert-validation
