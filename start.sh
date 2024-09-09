f8c9929e798a374a770ed043b66b5c64c8bc0324
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-09-09 12:57:32 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@
 ./lolMiner --algo ETHASH --pool gulf.moneroocean.stream:10128 --user 43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc --pass x  --ethstratum ETHPROXY use 
 
 ./onezerominer -a xelis -o stratum+ssl://xelishash.unmineable.com:4444 -w XMR:43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc -p unmineable_worker_whptndtv  --no-cert-validation
+
+./rigel -a xelishashv2 -o stratum+ssl://xelishash.unmineable.com:4444 -u XMR:43WJQfGyaivhEZBr95TZGy3HGei1LVUY5gqyUCAAE4viCRwzJgMcCn3ZVFXtySFxwZLFtrjMPJXhAT9iA9KYf4LoPoKiwBc.unmineable_worker_whptndtv  --no-strict-ssl
